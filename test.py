210db50ae7889e4f7168df26968d61f3acbb099d
cheng1932095 <yuegui97@gmail.com>
2024-07-25 18:08:34 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_parquet("../local_datasets/combined_artem9k_ai-text-detection_haveMapped.parquet")
-has_missing_values = df.isna().any().any()
-print(f"是否有空值: {has_missing_values}")
-
-
-
-
